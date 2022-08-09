acc5379d3a5d21fbf1d19c9a8405dcfbedd3e0b0
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-08-09 12:43:24 +0900

    feat: 🎸 gemやアプリの各種設定をアップデートした (#9)

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,30 @@
 require_relative "boot"
-
 require "rails/all"
-
 Bundler.require(*Rails.groups)
 
 module FreePlayground
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.time_zone = 'Tokyo'
+
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :utc
+
+    I18n.config.available_locales = %i[en ja]
+    I18n.default_locale = :ja
 
     config.generators do |g|
-      g.helper false
-      g.test_framework false
+      g.assets     false
+      g.helper     false
+
+      g.test_framework(
+        :rspec,
+        fixtures: true,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      )
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
