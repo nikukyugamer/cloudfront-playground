e593f912ef8804917f7726a6e8d7475fdb3d7f3c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-08-15 15:28:49 +0900

    feat: 🎸 アクセスと同時に Cookie を付与するページを追加した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 
 class HomeController < ApplicationController
+  before_action :set_common_instance_variables
   before_action :check_or_create_cloudfront_private_key
 
   def direct_image_link
@@ -12,19 +13,13 @@ class HomeController < ApplicationController
     )
   end
 
-  def index
-    @cookies = cookies.to_h
-    @request_host = request.host
+  def index; end
 
-    @min_tobus2_url = 'https://assets.neo-kobe-city.com/min_tobus2.jpg'
-    @min_tukimi3_url = 'https://assets.neo-kobe-city.com/min_tukimi3.jpg'
-    @min_undokai1_url = 'https://assets.neo-kobe-city.com/min_undokai1.jpg'
-    @min_up1_url = 'https://assets.neo-kobe-city.com/min_up1.jpg'
-    @min_xmas3_url = 'https://assets.neo-kobe-city.com/min_xmas3.jpg'
-    @min_yuyake3_url = 'https://assets.neo-kobe-city.com/min_yuyake3.jpg'
+  def trial_a
+    eat_cookies(without_subdomain: true)
   end
 
-  def eat_cookies
+  def eat_cookies(without_subdomain: false)
     check_or_create_cloudfront_private_key
 
     signer = Aws::CloudFront::CookieSigner.new(
@@ -53,13 +48,14 @@ class HomeController < ApplicationController
       policy: policy_statement.to_json
     )
 
+    cookie_domain = if without_subdomain || params[:cookie_domain] == 'without_subdomain'
+                      '.neo-kobe-city.com'
+                    else
+                      request.host
+                    end
+
     cookie_params.each do |k, v|
-      cookies[k] = case params[:cookie_domain]
-                   when 'without_subdomain'
-                     { value: v, domain: 'neo-kobe-city.com' }
-                   else
-                     { value: v } # domain: request.host
-                   end
+      cookies[k] = { value: v, domain: cookie_domain }
     end
   end
 
@@ -93,5 +89,17 @@ class HomeController < ApplicationController
     return if File.exist?(private_key_path)
 
     File.write(private_key_path, ENV.fetch('CLOUDFRONT_PRIVATE_KEY'))
+  end
+
+  def set_common_instance_variables
+    @cookies = cookies.to_h
+    @request_host = request.host
+
+    @min_tobus2_url = 'https://assets.neo-kobe-city.com/min_tobus2.jpg'
+    @min_tukimi3_url = 'https://assets.neo-kobe-city.com/min_tukimi3.jpg'
+    @min_undokai1_url = 'https://assets.neo-kobe-city.com/min_undokai1.jpg'
+    @min_up1_url = 'https://assets.neo-kobe-city.com/min_up1.jpg'
+    @min_xmas3_url = 'https://assets.neo-kobe-city.com/min_xmas3.jpg'
+    @min_yuyake3_url = 'https://assets.neo-kobe-city.com/min_yuyake3.jpg'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'eat_cookies', to: 'home#eat_cookies'
   get 'discard_cookies', to: 'home#discard_cookies'
   get 'direct_image_link', to: 'home#direct_image_link'
+
+  get 'trial_a', to: 'home#trial_a'
+  get 'trial_b', to: 'home#trial_b'
 end
